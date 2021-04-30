c0a4391d286974e7335e90a131b167f422e26223
rock-007 <umair.ashraf2019@gmail.com>
2021-04-30 17:39:12 +0100

    all test passing

--- part_2_code/tests/test_card_game.py
+++ part_2_code/tests/test_card_game.py
@@ -10,13 +10,13 @@ class card_gameTest(unittest.TestCase):
         self.second_card = Card("clubs",3)
         self.cards = [self.first_card, self.second_card]
 
-def test_check_for_ace(self):
-    self.assertEqual("Ace", self.cardGame.check_for_ace(self.first_card))
+    def test_check_for_ace(self):
+        self.assertEqual(True, self.cardGame.check_for_ace(self.first_card))
 
-def test_highest_card(self):
-    self.assertEqual(self.first_card, self.cardGame.higest_card(self.first_card, self.second_card))
+    def test_highest_card(self):
+        self.assertEqual("clubs", self.cardGame.highest_card(self.first_card, self.second_card).suit)
 
 
-def test_cards_total(self):
-    self.assertEqual(4, self.cardGame.higest_card(self.cards))
+    def test_cards_total(self):
+        self.assertEqual("You have a total of4", self.cardGame.cards_total(self.cards))
 
